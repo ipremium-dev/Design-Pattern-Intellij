9c7c299107f9ae17f2a9675052f35436be5dd971
ipremium-dev <149918425+ipremium-dev@users.noreply.github.com>
2025-07-20 16:44:35 +0800

    Initial commit

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Resources:
+
+https://www.tutorialspoint.com/design_pattern/index.htm
---
+++ src/com/pattern/factory/package-info.java
@@ -0,0 +1,3 @@
+package com.pattern.factory;
+
+//https://www.tutorialspoint.com/design_pattern/factory_pattern.htm
